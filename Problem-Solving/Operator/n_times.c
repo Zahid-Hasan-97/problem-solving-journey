078e3ff00232d86724ca00c34c4e796977fbe315
Zahid Hasan <md.zahid.hasan.topu@gmail.com>
2026-05-06 02:44:11 +0600

    added n_times.c

---
+++ Problem-Solving/Operator/n_times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int N;
+    scanf("%d", &N);
+    if(N > 0 ){
+        for (int i = 1; i <= N; i++){
+            printf("I Love N times\n");
+        }
+    }
+    return 0;
+}
