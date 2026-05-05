eea108d85ff302a02c5e931eff83fd7b79e3a795
Zahid Hasan <md.zahid.hasan.topu@gmail.com>
2026-05-06 01:14:24 +0600

    first commit

---
+++ Problem-Solving/Fundamentals/5-marks.c
@@ -0,0 +1,22 @@
+// Statement
+//             In this problem you will be given an integer number N.You will have to add 5 with N and print it.
+
+//         Constraints -
+//         1 <=
+//     N <= 100
+
+//          Input format
+//              Input consists of an integer number N.Output Format
+//                  Output the result after adding 5 with N.
+
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int add = N + 5;
+
+    printf("%d", add);
+    return 0;
+}
---
+++ Problem-Solving/Fundamentals/zero-or-non-zero.c
@@ -0,0 +1,26 @@
+// Statement
+// In this problem you will be given an integer number N. Print "Zero" if the number is equals to 0 and "Non Zero" Otherwise.
+
+// Constraints
+// -1000 <= N <= 1000
+// Input format
+// The input consists of an integer N.
+// Output Format
+// Print  Zero if the number is equals to 0 and Non Zero Otherwise.
+
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    if (N == 0)
+    {
+        printf("Zero");
+    }
+    else
+    {
+        printf("Non Zero");
+    }
+    return 0;
+}
