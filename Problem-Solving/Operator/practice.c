a755454c428821bf8991766bc7bbfeceb6ad2e6c
Zahid Hasan <md.zahid.hasan.topu@gmail.com>
2026-05-06 01:47:16 +0600

    added all folders practice.c

---
+++ Problem-Solving/Operator/practice.c
@@ -0,0 +1,14 @@
+// Statement
+//     I know that you're loving practice days! So this task is for you. You need to print "I Love Practice" without the quotation marks. I know that you can do it!
+
+//     Input format
+//         No input..
+
+//     Output Format Output "I Love Practice"
+
+#include <stdio.h>
+int main()
+{
+    printf("I Love Practice");
+    return 0;
+}
