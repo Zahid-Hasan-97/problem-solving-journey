cbe67f8bbeac23c1035b871eb5485abe17ef493f
Zahid Hasan <md.zahid.hasan.topu@gmail.com>
2026-05-18 23:39:15 +0600

    updated call_stack.c

--- C/recursion/call_stack.c
+++ C/recursion/call_stack.c
@@ -1,26 +1,31 @@
+// Untill calling the void function will not execute
+
 #include <stdio.h>
 
 void one()
 {
-    printf("One\n");
+    printf("One\n"); // 8. after entering one() function it will print "one"
 }
 
 void two()
 {
-    printf("Two\n");
+    printf("Two\n"); // 6. after entering two() function it will print "Two"
+
+    one(); // 7. Then it will call one() function
 }
 
-void Three() // Untill calling the void function will not execute
+void three()
 {
-    printf("Three");
+    printf("Three\n"); // 4. after entering three() function it will print "Three"
+
+    two(); // 5. Then it will call two() function
 }
 
-int main()
+int main() // 1. the main function will execute 
 {
-    printf("Call Stack\n");
+    printf("Call Stack\n"); // 2. It wil print "Call Stack"
 
-    one();
-    two();
+    three(); // 3. now it will call the three() function
 
     return 0;
 }
