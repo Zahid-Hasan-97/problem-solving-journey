ed33d20306a860321565731924b13d0bf3f0a3ae
Zahid Hasan <md.zahid.hasan.topu@gmail.com>
2026-06-02 03:33:15 +0600

    added Palindrome.c

--- I_Palindrome.c
+++ I_Palindrome.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     char s[1001];
+
     scanf("%s", s);
 
-    
+    int flag = 1;
 
+    for (int i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        if (s[i] != s[j])
+        {
+            flag = 0;
+        }
+    }
+
+    if (flag == 1)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
 
     return 0;
 }
